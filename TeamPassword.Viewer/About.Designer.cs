f1d6da51af105eb284fc9ea654dfb1aa2ca6eeb5
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2020-02-17 23:10:59 +0100

    new logo in about dialog

--- TeamPassword.Viewer/About.Designer.cs
+++ TeamPassword.Viewer/About.Designer.cs
@@ -28,102 +28,102 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pbLogo = new System.Windows.Forms.PictureBox();
-			this.lblVersion = new System.Windows.Forms.Label();
-			this.lblLibrary = new System.Windows.Forms.Label();
-			this.btnOk = new System.Windows.Forms.Button();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.lblCopyright = new System.Windows.Forms.Label();
-			((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
-			this.panel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// pbLogo
-			// 
-			this.pbLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblLibrary = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.pbLogo.Image = global::TeamPassword.Viewer.Properties.Resources.tpm;
-			this.pbLogo.InitialImage = null;
-			this.pbLogo.Location = new System.Drawing.Point(12, 12);
-			this.pbLogo.Name = "pbLogo";
-			this.pbLogo.Size = new System.Drawing.Size(401, 61);
-			this.pbLogo.TabIndex = 2;
-			this.pbLogo.TabStop = false;
-			// 
-			// lblVersion
-			// 
-			this.lblVersion.AutoSize = true;
-			this.lblVersion.Location = new System.Drawing.Point(21, 88);
-			this.lblVersion.Name = "lblVersion";
-			this.lblVersion.Size = new System.Drawing.Size(62, 13);
-			this.lblVersion.TabIndex = 3;
-			this.lblVersion.Text = "Version: {0}";
-			// 
-			// lblLibrary
-			// 
-			this.lblLibrary.AutoSize = true;
-			this.lblLibrary.Location = new System.Drawing.Point(21, 106);
-			this.lblLibrary.Name = "lblLibrary";
-			this.lblLibrary.Size = new System.Drawing.Size(95, 13);
-			this.lblLibrary.TabIndex = 4;
-			this.lblLibrary.Text = "Library version: {0}";
-			// 
-			// btnOk
-			// 
-			this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnOk.Location = new System.Drawing.Point(350, 11);
-			this.btnOk.Name = "btnOk";
-			this.btnOk.Size = new System.Drawing.Size(75, 23);
-			this.btnOk.TabIndex = 5;
-			this.btnOk.Text = "OK";
-			this.btnOk.UseVisualStyleBackColor = true;
-			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
-			// 
-			// panel1
-			// 
-			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.pbLogo.Image = global::TeamPassword.Viewer.Properties.Resources.tpm;
+            this.pbLogo.InitialImage = null;
+            this.pbLogo.Location = new System.Drawing.Point(12, 12);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(452, 61);
+            this.pbLogo.TabIndex = 2;
+            this.pbLogo.TabStop = false;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(21, 88);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(62, 13);
+            this.lblVersion.TabIndex = 3;
+            this.lblVersion.Text = "Version: {0}";
+            // 
+            // lblLibrary
+            // 
+            this.lblLibrary.AutoSize = true;
+            this.lblLibrary.Location = new System.Drawing.Point(21, 106);
+            this.lblLibrary.Name = "lblLibrary";
+            this.lblLibrary.Size = new System.Drawing.Size(95, 13);
+            this.lblLibrary.TabIndex = 4;
+            this.lblLibrary.Text = "Library version: {0}";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnOk.Location = new System.Drawing.Point(401, 11);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.panel1.BackColor = System.Drawing.SystemColors.Control;
-			this.panel1.Controls.Add(this.btnOk);
-			this.panel1.Location = new System.Drawing.Point(-12, 163);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(453, 74);
-			this.panel1.TabIndex = 6;
-			// 
-			// lblCopyright
-			// 
-			this.lblCopyright.AutoSize = true;
-			this.lblCopyright.Location = new System.Drawing.Point(21, 124);
-			this.lblCopyright.Name = "lblCopyright";
-			this.lblCopyright.Size = new System.Drawing.Size(71, 13);
-			this.lblCopyright.TabIndex = 7;
-			this.lblCopyright.Text = "Copyright: {0}";
-			// 
-			// About
-			// 
-			this.AcceptButton = this.btnOk;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.Window;
-			this.CancelButton = this.btnOk;
-			this.ClientSize = new System.Drawing.Size(425, 204);
-			this.Controls.Add(this.lblCopyright);
-			this.Controls.Add(this.panel1);
-			this.Controls.Add(this.lblLibrary);
-			this.Controls.Add(this.lblVersion);
-			this.Controls.Add(this.pbLogo);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "About";
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "About";
-			((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
-			this.panel1.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.btnOk);
+            this.panel1.Location = new System.Drawing.Point(-12, 163);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 74);
+            this.panel1.TabIndex = 6;
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.Location = new System.Drawing.Point(21, 124);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(71, 13);
+            this.lblCopyright.TabIndex = 7;
+            this.lblCopyright.Text = "Copyright: {0}";
+            // 
+            // About
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.btnOk;
+            this.ClientSize = new System.Drawing.Size(476, 204);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblLibrary);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.pbLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
