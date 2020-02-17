98c8e60cdf248673e7ff52fc19f24c67a9a84f8c
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2020-02-17 23:18:23 +0100

    version bump

--- TeamPassword.Viewer/Properties/AssemblyInfo.cs
+++ TeamPassword.Viewer/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.3.1620")]
-[assembly: AssemblyFileVersion("1.4.3.1620")]
+[assembly: AssemblyVersion("1.4.3.1630")]
+[assembly: AssemblyFileVersion("1.4.3.1630")]
